a0f98ad9f380891394c86f6c297b4da0bc1e1590
hongseok <yhongseok92@gmail.com>
2021-10-04 11:12:34 +0900

    test: (GameValidatorTest)Balls의 유효성을 검증하는 기능 테스트 코드 작성

---
+++ src/test/java/baseball/validator/GameValidatorTest.java
@@ -0,0 +1,48 @@
+package baseball.validator;
+
+import baseball.domain.baseball.Ball;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class GameValidatorTest {
+
+    List<Ball> balls = new ArrayList<>();
+
+    @Test
+    @DisplayName("리스트가 비어있는지 확인하기")
+    void isEmpty() {
+        Ball newBall = new Ball(5);
+        GameValidator validator = new GameValidator(balls, newBall);
+
+        assertThat(validator.isValidate()).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("리스트에 중복값이 있는지 확인하기")
+    void isContains() {
+        balls.add(new Ball(1));
+        balls.add(new Ball(2));
+        Ball newBall = new Ball(2);
+
+        GameValidator validator = new GameValidator(balls, newBall);
+
+        assertThat(validator.isValidate()).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("리스트에 중복값이 없는지 확인하기")
+    void isNotContains() {
+        balls.add(new Ball(1));
+        balls.add(new Ball(2));
+        Ball newBall = new Ball(5);
+
+        GameValidator validator = new GameValidator(balls, newBall);
+
+        assertThat(validator.isValidate()).isEqualTo(true);
+    }
+}
